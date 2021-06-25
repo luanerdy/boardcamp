e5c0c41f578359f2217b8a55241a301a907fae27
vagnerpavani <vagner.alves.pavani@gmail.com>
2021-06-25 14:15:04 -0300

    preparando review

--- src/routes/categories/categories.js
+++
@@ -1,9 +0,0 @@
-import addCategory from "./addCategory.js";
-import getCategories from "./getCategories.js";
-
-const categories = (app, connection) => {
-    getCategories(app, connection);
-    addCategory(app, connection);
-};
-
-export default categories;
